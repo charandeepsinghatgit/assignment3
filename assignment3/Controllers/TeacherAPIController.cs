b75af154553ab4109b91bcefcca8e90254256aad
charandeep singh <N01748813@humber.ca>
2025-07-17 19:55:12 -0400

    Add project files.

---
+++ assignment3/Controllers/TeacherAPIController.cs
@@ -0,0 +1,43 @@
+﻿using assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace assignment3.Controllers
+{
+    [Route("api/teacher")]
+    [ApiController]
+    public class TeacherAPIController : ControllerBase
+    {
+        SchoolDbContext School = new SchoolDbContext();
+        [HttpGet(template: "getTeacherList")]
+        public List<Teacher> GetTeacherInfo()
+        {
+            //Store it to a list
+            List<Teacher> Example = new List<Teacher>();
+
+            using (MySqlConnection Connection = School.AccessDatabase())
+            {
+
+                Connection.Open();
+
+                MySqlCommand Command = Connection.CreateCommand();
+
+                Command.CommandText = "select * from teachers";
+
+                using(MySqlDataReader ReadResult = Command.ExecuteReader())
+
+                while (ReadResult.Read())
+                {
+                    Teacher Teacher = new Teacher();
+                    Teacher.TeacherId = Convert.ToInt32(ReadResult["teacherid"]);
+                    Teacher.TeacherFName = ReadResult["teacherfname"].ToString();
+                    Teacher.TeacherLName = ReadResult["teacherlname"].ToString();
+                    Example.Add(Teacher);
+
+                }
+                
+            }
+            return Example;
+        }
+    }
+}
